0cf029eaca072bab3d2853b7d2b22081a0488599
mullaart <m.mullaart.soderholm@gmail.com>
2022-09-21 15:35:00 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='PythonFaultTolerance',
+      version='1.0',
+      description='Python Fault Tolerance',
+      author='Mr. Sniffles',
+      author_email='sniffles@catswithtails.testnet',
+      url='No',
+      packages=['ftol'],
+     )
